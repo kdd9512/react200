e1c336d8eab0c88bbf53d16534ffbade2dd777f8
kdd9512 <hyunwoo8805@gmail.com>
2021-11-06 23:27:25 +0900

    20211106 1st commit.
    React 200
    number 1~5

---
+++ src/R003_Component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class R003_Component extends Component {
+    render() {
+        return (
+            <h2>3. Component 사용하기</h2>
+        )
+    };
+}
+
+export default R003_Component;
---
+++ src/R004_UsingRender.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class R004_UsingRender extends Component {
+    render() {
+        console.log("4. render call")
+        return (
+            <h2>4. render 사용하기.</h2>
+        )
+    };
+}
+
+export default R004_UsingRender;
